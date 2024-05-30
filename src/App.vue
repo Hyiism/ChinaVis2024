99cca1ed4895f34b9bfd5e99e9536d94ed29f7f4
hy <aishiminhy@163.com>
2024-05-30 17:11:23 +0800

    最新代码

--- src/App.vue
+++ src/App.vue
@@ -1,101 +1,141 @@
 <template>
-  <div id="app">
-
-    <div id="LeftViewer">
-      <LeftViewer/>
-    </div>
-
-    <div id="ModelViewer">
-      <ModelViewer/>
-    </div>
-
-    <div id="RightViewer">
-      <RightViewer/>
-    </div>
-
-    <div id="BottomViewer">
-      <BottomViewer/>
-    </div>
-
+  <div class="chart-container">
+    <div id="main"> </div>
   </div>
-
 </template>
-
+<!-- 全部推 -->
 <script>
-import ModelViewer from './components/ModelViewer.vue';
-import LeftViewer from './components/LeftViewer.vue';
-import RightViewer from './components/RightViewer.vue';
-import BottomViewer from './components/BottomViewer.vue';
+import * as echarts from 'echarts';
 
 export default {
-  name: 'App',
-  components: {
-    ModelViewer,
-    LeftViewer,
-    RightViewer,
-    BottomViewer
+  name: 'EChartsComponent',
+  mounted() {
+    this.initChart();
+    window.addEventListener('resize', this.resizeChart);
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.resizeChart);
+  },
+  methods: {
+    initChart() {
+      var chartDom = document.getElementById('main');
+      var myChart = echarts.init(chartDom);
+      var option = {
+          title: {
+              text: '学生综合成绩排行榜',
+              left: 'center',
+          },
+
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        legend: {
+          top: '7%'
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          // bottom: '3%',
+          top: '15%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'value'
+        },
+        yAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        series: [
+          {
+            name: 'subject_1',
+            type: 'bar',
+            stack: 'total',
+            label: {
+              show: true
+            },
+            emphasis: {
+              focus: 'series'
+            },
+            data: [320, 302, 301, 334, 390, 330, 320]
+          },
+          {
+            name: 'subject_2',
+            type: 'bar',
+            stack: 'total',
+            label: {
+              show: true
+            },
+            emphasis: {
+              focus: 'series'
+            },
+            data: [120, 132, 101, 134, 90, 230, 210]
+          },
+          {
+            name: 'subject_3',
+            type: 'bar',
+            stack: 'total',
+            label: {
+              show: true
+            },
+            emphasis: {
+              focus: 'series'
+            },
+            data: [220, 182, 191, 234, 290, 330, 310]
+          },
+          {
+            name: 'subject_4',
+            type: 'bar',
+            stack: 'total',
+            label: {
+              show: true
+            },
+            emphasis: {
+              focus: 'series'
+            },
+            data: [150, 212, 201, 154, 190, 330, 410]
+          },
+          {
+            name: 'subject_5',
+            type: 'bar',
+            stack: 'total',
+            label: {
+              show: true
+            },
+            emphasis: {
+              focus: 'series'
+            },
+            data: [820, 832, 901, 934, 1290, 1330, 1320]
+          }
+        ]
+      };
+      option && myChart.setOption(option);
+      this.myChart = myChart;
+    },
+    resizeChart() {
+      if (this.myChart) {
+        this.myChart.resize();
+      }
+    }
   }
 };
 </script>
 
-
-<style>
-
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box; /* Ensure padding and border are included in the element's total width and height */
+<style scoped>
+.chart-container {
+  position: relative;
+  width: 100%;
+  height: 90%;
 }
 
-#app {
-  display: flex;
-  flex-direction: column; /* Arrange children in a column */
-  align-items: center; /* Center align horizontally */
-  height: 100vh;
-  margin: 0;
-  box-sizing: border-box; /* Ensure padding is included in the height */
-}
-
-#ModelViewer {
-  width: 60%;
-  height: 60%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 0px; /* Adjust this value to control the space below */
-}
-
-#LeftViewer {
-  width: 20%;
-  height: 60%;
+#main {
   position: absolute;
   top: 0;
   left: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #26595f; /* 可以根据需要设置背景颜色 */
-}
-
-#RightViewer {
-  width: 20%;
-  height: 60%;
-  position: absolute;
-  top: 0;
-  right: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #26595f; /* 可以根据需要设置背景颜色 */
-}
-
-#BottomViewer {
   width: 100%;
-  height: 40%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #07ab8d; /* Adjust as needed */
+  height: 100%;
 }
-
 </style>
