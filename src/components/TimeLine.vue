26219e972ec85ce4b389afb16bbd880381e6f48a
hy <aishiminhy@163.com>
2024-06-05 19:39:16 +0800

    添加时间选择刷子，模拟将数据传给后端

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,252 +1,188 @@
-<!-- <template>
-    <div id="timeline" ref="timeLine" class="time-line">
-        <svg ref="svgElement"></svg>
+<template>
+    <div ref="chartContainer" style="width: 100%; height: 100%;">
+        <div class="tooltip" v-show="tooltipVisible" :style="tooltipStyle">{{ tooltipText }}</div>
+        <div class="tooltipStart" v-show="tooltipVisibleStart" :style="tooltipStyleStart">{{ tooltipTextStart }}</div>
+        <div class="tooltipEnd" v-show="tooltipVisibleEnd" :style="tooltipStyleEnd">{{ tooltipTextEnd }}</div>
     </div>
 </template>
 
 <script>
 import * as d3 from 'd3';
+import axios from 'axios';
 
 export default {
-    name: 'TimeLine',
+    name: 'TimeSelectionChart',
     data() {
-        return {};
+        return {
+            tooltipVisible: false,
+            tooltipText: '',
+            tooltipStyle: {
+                position: 'absolute',
+                textAlign: 'center',
+                width: '50px',
+                height: '27px',
+                padding: '2px',
+                font: '12px sans-serif',
+                background: 'white',
+                border: '0px',
+                borderRadius: '8px',
+                pointerEvents: 'none',
+                opacity: 1,
+                left: '0px',
+                top: '0px'
+            },
+            tooltipVisibleStart: false,
+            tooltipTextStart: '',
+            tooltipStyleStart: {
+                position: 'absolute',
+                textAlign: 'center',
+                width: '50px',
+                height: '27px',
+                padding: '2px',
+                font: '12px sans-serif',
+                background: 'white',
+                border: '0px',
+                borderRadius: '8px',
+                left: '0px',
+                top: '0px',
+            },
+            tooltipVisibleEnd: false,
+            tooltipTextEnd: '',
+            tooltipStyleEnd: {
+                position: 'absolute',
+                textAlign: 'center',
+                width: '50px',
+                height: '27px',
+                padding: '2px',
+                font: '12px sans-serif',
+                background: 'white',
+                border: '0px',
+                borderRadius: '8px',
+                zIndex: 1
+            }
+        };
     },
     mounted() {
         this.drawChart();
     },
     methods: {
         drawChart() {
-            const margin = { top: 0, right: 30, bottom: 30, left: 0 };
-            const width = this.$refs.timeLine.clientWidth;
-            const height = this.$refs.timeLine.clientHeight;
+            const margin = { top: 0, right: 0, bottom: 0, left: 0 };
+            const chartContainerWidth = this.$refs.chartContainer.clientWidth;
+            const width = chartContainerWidth;
+            const height = 65;
+            const svg = d3.select(this.$refs.chartContainer)
+                .append('svg')
+                .attr('width', chartContainerWidth)
+                .attr('height', height + margin.top + margin.bottom)
+                .append('g')
+                .attr('transform', `translate(${margin.left},${margin.top})`);
 
-            const svg = d3.select(this.$refs.svgElement)
-                .attr("width", width)
-                .attr("height", height)
-                .append("g")
-                .attr("transform", `translate(${margin.left},${margin.top})`);
-
-            // Add background rect
-            svg.append("rect")
-                .attr("class", "background")
+            svg.append('rect')
+                .attr('class', 'background')
                 .style("fill", "#F29897")
-                .attr("x", 0)
-                .attr("y", 0)
-                .attr("width", width)
-                .attr("height", height);
+                .attr('x', 0)
+                .attr('y', 0)
+                .attr('width', chartContainerWidth)
+                .attr('height', height);
 
-            const tooltip = d3.select("#timeline").append("div")
-                .attr("class", "tooltip")
-                .style("opacity", 0);
-
-            // Generate more random data
             const data = d3.range(500).map((d, i) => ({
                 date: new Date(2023, 3, 13, 0, i * 1, 0),
                 value: Math.random()
             }));
 
-            // X axis
             const x = d3.scaleTime()
                 .domain(d3.extent(data, d => d.date))
                 .range([0, width]);
 
-            // Y axis
             const y = d3.scaleLinear()
                 .domain([0, 1])
                 .range([height, 0]);
 
-            // Add the bars
-            svg.selectAll(".bar")
+            svg.selectAll('.bar')
                 .data(data)
-                .enter().append("rect")
-                .attr("class", "bar")
+                .enter().append('rect')
+                .attr('class', 'bar')
                 .style("fill", "#AAFAAA")
-                .attr("x", d => x(d.date))
-                .attr("width", width / data.length) // 柱子更细，且无间隙
-                .attr("y", d => y(d.value))
-                .attr("height", d => height - y(d.value))
-                .on("mouseover", function(event, d) {
-                    tooltip.transition()
-                        .duration(200)
-                        .style("opacity", .9);
-                    tooltip.html(d.date.toLocaleTimeString())
-                        .style("left", (event.pageX + 5) + "px")
-                        .style("top", (event.pageY - 28) + "px");
+                .attr('x', d => x(d.date))
+                .attr('width', width / data.length)
+                .attr('y', d => y(d.value))
+                .attr('height', d => height - y(d.value))
+                .on('mouseover', (event, d) => {
+                    this.tooltipVisible = true;
+                    this.tooltipText = d.date.toLocaleTimeString();
+                    this.tooltipStyle.left = (event.pageX + 5) + 'px';
+                    this.tooltipStyle.top = (event.pageY - 28) + 'px';
                 })
-                .on("mouseout", function() {
-                    tooltip.transition()
-                        .duration(500)
-                        .style("opacity", 0);
+                .on('mouseout', () => {
+                    this.tooltipVisible = false;
+                });
+            let startXPosition = null;
+            let startYPosition = null;
+
+            // 添加刷子
+            const brush = d3.brushX()
+                .extent([[0, 0], [width, height]])
+                .on('start', (event) => {
+                    // 获取刷子的起始位置
+                    const [mouseXStart, mouseYStart] = d3.pointer(event, window);
+                    console.log('Brush start position:', mouseXStart, mouseYStart);
+                    startXPosition = mouseXStart;
+                    startYPosition = mouseYStart;
+                    console.log('Brush start position:', startXPosition, startYPosition);
+                })
+                .on('brush', (event) => {
+                    const selection = event.selection;
+                    if (selection) {
+                        const [x0, x1] = selection.map(x.invert);
+                        const [mouseXEnd, mouseYEnd] = d3.pointer(event, window);
+                        // 显示悬浮时间
+                        this.tooltipVisibleStart = true;
+                        this.tooltipVisibleEnd = true;
+                        this.tooltipStyleStart.left = startXPosition + 'px';
+                        this.tooltipStyleStart.top = startYPosition + 'px';
+
+                        this.tooltipStyleEnd.left = mouseXEnd + 'px';
+                        this.tooltipStyleEnd.top = mouseYEnd + 'px';
+                        this.tooltipTextStart = `${x0.toLocaleTimeString()}`;
+                        this.tooltipTextEnd = `${x1.toLocaleTimeString()}`;
+                    }
+                })
+                .on('end', (event) => {
+                    const selection = event.selection;
+                    if (selection) {
+                        const [x0, x1] = selection.map(x.invert);
+                        console.log('Brush ended:', x0, x1);
+                        // 调用发送数据到后端的方法
+                        this.sendDataToBackend(x0, x1);
+                        this.tooltipVisibleStart = false;
+                        this.tooltipVisibleEnd = false;
+                    }
                 });
 
-            // Add brushing (uncomment if needed)
-            // svg.append("g")
-            //     .attr("class", "brush")
-            //     .call(d3.brushX()
-            //         .extent([[0, 0], [width, height]])
-            //         .on("end", this.updateChart))
-            //     .on("mouseover", function(event, d) {
-            //         console.log("Mouse over:", d);
-            //     })
-            //     .on("click", function(event, d) {
-            //         console.log("Mouse out:", d);
-            //     });
+            svg.append('g')
+                .attr('class', 'brush')
+                .call(brush);
         },
-        updateChart() {
-            // Brushing functionality (if needed)
+        sendDataToBackend(startTime, endTime) {
+            // 使用axios发送数据到后端
+            axios.post('', {
+                startTime: startTime.toLocaleTimeString(),
+                endTime: endTime.toLocaleTimeString()
+            })
+                .then(response => {
+                    console.log('Data sent successfully:', response.data);
+                })
+                .catch(error => {
+                    console.error('Error sending data:', error);
+                });
         }
+
     }
 };
 </script>
 
 <style scoped>
-.time-line {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-}
-
-#timeline .tooltip {
-    position: absolute;
-    text-align: center;
-    width: 60px;
-    height: 18px;
-    padding: 2px;
-    font: 12px sans-serif;
-    background: white;
-    border: 0px;
-    border-radius: 8px;
-    pointer-events: none;
-    opacity: 0;
-}
-</style> -->
-
-<template>
-    <div ref="chartContainer" style="width: 100%; height: 100%;">
-      <div class="tooltip" v-show="tooltipVisible" :style="tooltipStyle">{{ tooltipText }}</div>
-    </div>
-  </template>
-  
-  <script>
-  import * as d3 from 'd3';
-  
-  export default {
-    name: 'TimeSelectionChart',
-    data() {
-      return {
-        tooltipVisible: false,
-        tooltipText: '',
-        tooltipStyle: {
-          position: 'absolute',
-          textAlign: 'center',
-          // width: '100%',
-          // height: '100%',
-          // padding: '0',
-          width: '50px',
-          height: '27px',
-          padding: '2px',
-          font: '12px sans-serif',
-          background: 'white',
-          border: '0px',
-          borderRadius: '8px',
-          pointerEvents: 'none',
-          opacity: 1,
-          left: '0px',
-          top: '0px'
-        }
-      };
-    },
-    mounted() {
-      this.drawChart();
-    },
-    methods: {
-      drawChart() {
-        // const margin = { top: 28, right: 30, bottom: 30, left: 0 };
-        const margin = { top: 0, right: 0, bottom: 0, left: 0 };
-        const chartContainerWidth = this.$refs.chartContainer.clientWidth;
-        // const width = chartContainerWidth - margin.left - margin.right;
-        // const height = 100 - margin.top - margin.bottom;
-        const width = chartContainerWidth;
-        const height = 65;
-        // console.log(this.$refs.chartContainer.clientWidth)
-  
-        const svg = d3.select(this.$refs.chartContainer)
-          .append('svg')
-          .attr('width', chartContainerWidth)
-          .attr('height', height + margin.top + margin.bottom)
-          .append('g')
-          .attr('transform', `translate(${margin.left},${margin.top})`);
-  
-        svg.append('rect')
-          .attr('class', 'background')
-          .style("fill", "#F29897")
-          .attr('x', 0)
-          .attr('y', 0)
-          .attr('width', chartContainerWidth)
-          .attr('height', height);
-  
-        const data = d3.range(500).map((d, i) => ({
-          date: new Date(2023, 3, 13, 0, i * 1, 0),
-          value: Math.random()
-        }));
-  
-        const x = d3.scaleTime()
-          .domain(d3.extent(data, d => d.date))
-          .range([0, width]);
-  
-        const y = d3.scaleLinear()
-          .domain([0, 1])
-          .range([height, 0]);
-  
-        svg.selectAll('.bar')
-          .data(data)
-          .enter().append('rect')
-          .attr('class', 'bar')
-          .style("fill", "#AAFAAA")
-          .attr('x', d => x(d.date))
-          .attr('width', width / data.length)
-          .attr('y', d => y(d.value))
-          .attr('height', d => height - y(d.value))
-          .on('mouseover', (event, d) => {
-            this.tooltipVisible = true;
-            this.tooltipText = d.date.toLocaleTimeString();
-            this.tooltipStyle.left = (event.pageX + 5) + 'px';
-            this.tooltipStyle.top = (event.pageY - 28) + 'px';
-          })
-          .on('mouseout', () => {
-            this.tooltipVisible = false;
-          });
-  
-        // const brush = d3.brushX()
-        //   .extent([[0, 0], [width, height]])
-        //   .on('end', this.updateChart);
-  
-        // svg.append('g')
-        //   .attr('class', 'brush')
-        //   .call(brush);
-      },
-      updateChart(event) {
-        const extent = event.selection;
-        if (!extent) return;
-  
-        const x = d3.scaleTime().domain(d3.extent(data, d => d.date)).range([0, width]);
-        const selectedTimeRange = [
-          x.invert(extent[0]),
-          x.invert(extent[1])
-        ];
-  
-        console.log("Selected time range:", selectedTimeRange);
-      }
-    }
-  };
-  </script>
-  
-  <style scoped>
-
 .chartContainer {
     width: 100%;
     height: 100%;
@@ -256,30 +192,17 @@ export default {
     position: relative;
 }
 
-  .tooltip {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    opacity: 0;
-  }
-  
-  .brush .selection {
+.brush .selection {
     fill: #8884d8;
     fill-opacity: 0.5;
-  }
-  
-  .bar {
+}
+
+.bar {
     fill: #AAFAAA;
-  }
-/*   
-  .border {
-    stroke: #000;
-    stroke-width: 1px;
-    fill: #f8f8f8;
-  } */
-  
-  .background {
+}
+
+
+.background {
     fill: #F29897;
-  }
-  </style>
-  
+}
+</style>
