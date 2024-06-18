ad06b5f345bd2752dad251e2bbe5a03bb82e895c
lkx <2875715920@qq.com>
2024-06-18 09:49:31 +0800

    1

--- src/components/StudentViewer/BottomRightViewerStudent.vue
+++ src/components/StudentViewer/BottomRightViewerStudent.vue
@@ -1,271 +1,266 @@
 <template>
-  <!-- 变换图 展示总体知识点和题目情况 -->
-  <div>
-    <!-- <p>班级视图BottomLeft test</p> -->
-    <div ref="chart" class="chart-container"></div>
-    <!-- <p>班级视图BottomLeft test</p> -->
-    <div ref="chart" class="chart-container"></div>
+  <div ref="chartContainer" class="chart-container">
+    <svg ref="chart"></svg>
   </div>
 </template>
 
 <script>
 import * as d3 from 'd3';
-import axios from 'axios';
 
 export default {
-  name: 'StackedLineChart',
+  name: 'SunburstChart',
   data() {
     return {
-      // data: [
-      //   { date: new Date(2020, 0, 1), industry: 'A', unemployed: 100 },
-      //   { date: new Date(2020, 0, 1), industry: 'B', unemployed: 200 },
-      //   { date: new Date(2020, 0, 1), industry: 'C', unemployed: 300 },
-      //   { date: new Date(2020, 1, 1), industry: 'A', unemployed: 150 },
-      //   { date: new Date(2020, 1, 1), industry: 'B', unemployed: 250 },
-      //   { date: new Date(2020, 1, 1), industry: 'C', unemployed: 350 },
-      //   { date: new Date(2020, 2, 1), industry: 'A', unemployed: 200 },
-      //   { date: new Date(2020, 2, 1), industry: 'B', unemployed: 100 },
-      //   { date: new Date(2020, 2, 1), industry: 'C', unemployed: 250 },
-      //   { date: new Date(2020, 3, 1), industry: 'A', unemployed: 50 },
-      //   { date: new Date(2020, 3, 1), industry: 'B', unemployed: 350 },
-      //   { date: new Date(2020, 3, 1), industry: 'C', unemployed: 450 },
-      //   { date: new Date(2020, 4, 1), industry: 'A', unemployed: 300 },
-      //   { date: new Date(2020, 4, 1), industry: 'B', unemployed: 400 },
-      //   { date: new Date(2020, 4, 1), industry: 'C', unemployed: 540 },
-      //   { date: new Date(2020, 5, 1), industry: 'A', unemployed: 300 },
-      //   { date: new Date(2020, 5, 1), industry: 'B', unemployed: 450 },
-      //   { date: new Date(2020, 5, 1), industry: 'C', unemployed: 100 },
-      //   { date: new Date(2020, 6, 1), industry: 'A', unemployed: 400 },
-      //   { date: new Date(2020, 6, 1), industry: 'B', unemployed: 50 },
-      //   { date: new Date(2020, 6, 1), industry: 'C', unemployed: 80 },
-      //   { date: new Date(2020, 7, 1), industry: 'A', unemployed: 450 },
-      //   { date: new Date(2020, 7, 1), industry: 'B', unemployed: 120 },
-      //   { date: new Date(2020, 7, 1), industry: 'C', unemployed: 650 },
-      //   { date: new Date(2020, 8, 1), industry: 'A', unemployed: 500 },
-      //   { date: new Date(2020, 8, 1), industry: 'B', unemployed: 200 },
-      //   { date: new Date(2020, 8, 1), industry: 'C', unemployed: 700 },
-      //   { date: new Date(2020, 9, 1), industry: 'A', unemployed: 50 },
-      //   { date: new Date(2020, 9, 1), industry: 'B', unemployed: 650 },
-      //   { date: new Date(2020, 9, 1), industry: 'C', unemployed: 750 },
-      //   { date: new Date(2020, 10, 1), industry: 'A', unemployed: 500 },
-      //   { date: new Date(2020, 10, 1), industry: 'B', unemployed: 700 },
-      //   { date: new Date(2020, 10, 1), industry: 'C', unemployed: 800 },
-      //   { date: new Date(2020, 11, 1), industry: 'A', unemployed: 320 },
-      //   { date: new Date(2020, 11, 1), industry: 'B', unemployed: 750 },
-      //   { date: new Date(2020, 11, 1), industry: 'C', unemployed: 200 },
-      // ]
-      data: []
+      preferdata: {
+        name: "root",
+        knowledge: [
+          {
+            name: "r8S3g",
+            subknowledge: [
+              {
+                name: "r8S3g_l0p5viby",
+                title: [
+                  { name: "Question_VgKw8PjY1FR6cm2QI9XW", value: 12 },
+                  { name: "Question_q7OpB2zCMmW9wS8uNt3H", value: 12 },
+                ]
+              },
+              {
+                name: "r8S3g_n0m9rsw4",
+                title: [
+                  { name: "Question_q7OpB2zCMmW9wS8uNt3H", value: 12 },
+                  { name: "Question_fZrP3FJ4ebUogW9V7taS", value: 12 },
+                  { name: "Question_BW0ItEaymH3TkD6S15JF", value: 12 },
+                  { name: "Question_rvB9mVE6Kbd8jAY4NwPx", value: 12 },
+                ]
+              },
+            ]
+          },
+          {
+            name: "t5V9e",
+            subknowledge: [
+              {
+                name: "t5V9e_e1k6cixp",
+                title: [
+                  { name: "Question_3oPyUzDmQtcMfLpGZ0jW", value: 12 },
+                  { name: "Question_3MwAFlmNO8EKrpY5zjUd", value: 12 },
+                  { name: "Question_x2L7AqbMuTjCwPFy6vNr", value: 12 },
+                  { name: "Question_tgOjrpZLw4RdVzQx85h6", value: 12 },
+                  { name: "Question_s6VmP1G4UbEQWRYHK9Fd", value: 12 },
+                ]
+              },
+            ]
+          },
+          {
+            name: "m3D1v",
+            subknowledge: [
+              {
+                name: "m3D1v_r1d7fr3l",
+                title: [
+                  { name: "Question_h7pXNg80nJbw1C4kAPRm", value: 12 },
+                  { name: "Question_6RQj2gF3OeK5AmDvThUV", value: 12 },
+                  { name: "Question_4nHcauCQ0Y6Pm8DgKlLo", value: 12 },
+                  { name: "Question_TmKaGvfNoXYq4FZ2JrBu", value: 12 },
+                  { name: "Question_NixCn84GdK2tySa5rB1V", value: 12 },
+                  { name: "Question_n2BTxIGw1Mc3Zo6RLdUe", value: 12 },
+                  { name: "Question_QRm48lXxzdP7Tn1WgNOf", value: 12 },
+                  { name: "Question_pVKXjZn0BkSwYcsa7C31", value: 12 },
+                  { name: "Question_oCjnFLbIs4Uxwek9rBpu", value: 12 },
+                ]
+              },
+              {
+                name: "m3D1v_v3d9is1x",
+                title: [
+                  { name: "Question_7NJzCXUPcvQF4Mkfh9Wr", value: 12 },
+                  { name: "Question_ZTbD7mxr2OUp8Fz6iNjy", value: 12 },
+                  { name: "Question_x2L7AqbMuTjCwPFy6vNr", value: 12 },
+                  { name: "Question_tgOjrpZLw4RdVzQx85h6", value: 12 },
+                  { name: "Question_s6VmP1G4UbEQWRYHK9Fd", value: 12 },
+                ]
+              },
+              {
+                name: "m3D1v_t0v5ts9h",
+                title: [
+                  { name: "Question_Jr4Wz5jLqmN01KUwHa7g", value: 12 },
+                ]
+              },
+            ]
+          },
+          {
+            name: "y9W5d",
+            subknowledge: [
+              {
+                name: "y9W5d_c0w4mj5h",
+                title: [
+                  { name: "Question_QRm48lXxzdP7Tn1WgNOf", value: 12 },
+                  { name: "Question_pVKXjZn0BkSwYcsa7C31", value: 12 },
+                  { name: "Question_Ej5mBw9rsOUKkFycGvz2", value: 12 },
+                  { name: "Question_lU2wvHSZq7m43xiVroBc", value: 12 },
+                  { name: "Question_Mh4CZIsrEfxkP1wXtOYV", value: 12 },
+                  { name: "Question_62XbhBvJ8NUSnApgDL94", value: 12 },
+                  { name: "Question_x2Fy7rZ3SwYl9jMQkpOD", value: 12 },
+                  { name: "Question_UXqN1F7G3Sbldz02vZne", value: 12 },
+                ]
+              },
+              {
+                name: "y9W5d_p8g6dgtv",
+                title: [
+                  { name: "Question_Ou3f2Wt9BqExm5DpN7Zk", value: 12 },
+                  { name: "Question_Az73sM0rHfWVKuc4X2kL", value: 12 },
+                ]
+              },
+              {
+                name: "y9W5d_e2j7p95s",
+                title: [
+                  { name: "Question_EhVPdmlB31M8WKGqL0wc", value: 12 },
+                ]
+              },
+            ]
+          },
+          {
+            name: "k4W1c",
+            subknowledge: [
+              {
+                name: "k4W1c_h5r6nux7",
+                title: [
+                  { name: "Question_lU2wvHSZq7m43xiVroBc", value: 12 },
+                ]
+              },
+            ]
+          },
+          {
+            name: "s8Y2f",
+            subknowledge: [
+              {
+                name: "s8Y2f_v4x8by9j",
+                title: [
+                  { name: "Question_x2Fy7rZ3SwYl9jMQkpOD", value: 12 },
+                ]
+              },
+            ]
+          },
+          {
+            name: "g7R2j",
+            subknowledge: [
+              {
+                name: "g7R2j_e0v1yls8",
+                title: [
+                  { name: "Question_oCjnFLbIs4Uxwek9rBpu", value: 12 },
+                  { name: "Question_5fgqjSBwTPG7KUV3it6O", value: 12 },
+                  { name: "Question_X3wF8QlTyi4mZkDp9Kae", value: 12 },
+                  { name: "Question_xqlJkmRaP0otZcX4fK3W", value: 12 },
+                  { name: "", value: 12 },
+                ]
+              },
+              {
+                name: "g7R2j_j1g8gd3v",
+                title: [
+                  { name: "Question_YWXHr4G6Cl7bEm9iF2kQ", value: 12 },
+                ]
+              },
+            ]
+          },
+          {
+            name: "b3C9s",
+            subknowledge: [
+              {
+                name: "b3C9s_l4z6od7y",
+                title: [
+                  { name: "Question_bumGRTJ0c8p4v5D6eHZa", value: 12 },
+                  { name: "Question_hZ5wXofebmTlzKB1jNcP", value: 12 },
+                ]
+              },
+              {
+                name: "b3C9s_j0v1yls8",
+                title: [
+                  { name: "Question_FNg8X9v5zcbB1tQrxHR3", value: 12 },
+                ]
+              },
+            ]
+          },
+        ]
+      }
     };
   },
   mounted() {
-    this.fetchData();
+    this.drawChart();
+    window.addEventListener('resize', this.drawChart);
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.drawChart);
   },
   methods: {
-    async fetchData() {
-      try {
-        const response = await axios.get('http://localhost:5000/get_student_scores_data');
-        const data = response.data;
-        this.processData(data);
-        this.createChart();
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+    drawChart() {
+      const container = this.$refs.chartContainer;
+      const svg = d3.select(this.$refs.chart);
+
+      const width = container.clientWidth;
+      const height = container.clientHeight;
+      const radius = Math.min(width, height) / 2.2;
+
+      svg.selectAll("*").remove();
+
+      const processData = (data) => {
+        const convert = (node) => {
+          if (node.title) {
+            // return { 
+            //   name: node.name, 
+            //   value: node.title.reduce((sum, t) => sum + t.value, 0) 
+            // };
+            return {
+              name: node.name,
+              children: node.title.map(titleNode => ({
+                name: titleNode.name,
+                value: titleNode.value
+              }))
+            };
+          }
+          const children = node.knowledge || node.subknowledge;
+          return { 
+            name: node.name, 
+            children: children.map(convert) 
+          };
+        };
+        return convert(data);
+      };
+
+      const rootData = processData(this.preferdata);
+      const root = d3.hierarchy(rootData).sum(d => d.value);
+
+      const partition = d3.partition().size([2 * Math.PI, radius]);
+
+      partition(root);
+
+      const color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, root.children.length + 1));
+
+      const arc = d3.arc()
+        .startAngle(d => d.x0)
+        .endAngle(d => d.x1)
+        .innerRadius(d => d.y0)
+        .outerRadius(d => d.y1);
+
+      svg
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', `translate(${width / 2},${height / 2})`)
+        .selectAll('path')
+        .data(root.descendants())
+        .enter().append('path')
+        .attr('d', arc)
+        .style('fill', d => color(d.data.name))
+        .append('title')
+        .text(d => `${d.data.name}\n${d.value}`);
     },
-    processData(data) {
-      const formattedData = [];
-      data.forEach(row => {
-        // 遍历 row 的所有属性
-        Object.keys(row).forEach(key => {
-            // 检查属性名是否匹配 dateYYYYMMDD 的模式
-            const match = key.match(/^date(\d{4})(\d{2})(\d{2})$/);
-            if (match) {
-                // 提取年份、月份和日期
-                const year = parseInt(match[1], 10);
-                const month = parseInt(match[2], 10) - 1; // JavaScript 中的月份是从 0 开始的
-                const day = parseInt(match[3], 10);
-                // 将数据添加到 formattedData 中
-                formattedData.push({ 
-                    industry: row.knowledge_id, 
-                    date: new Date(year, month, day), 
-                    unemployed: row[key] 
-                });
-            }
-        });
-        // formattedData.push({ industry: row.student_id, date: new Date(2024, 6, 1), unemployed: row.date20240601 });
-        // formattedData.push({ industry: row.student_id, date: new Date(2024, 6, 2), unemployed: row.date20240602 });
-      });
-      this.data = formattedData;
-    },
-    createChart() {
-      const data = this.data;
-
-      const width = 928;
-      const height = 550;
-      const marginTop = 50;
-      const marginRight = 10;
-      const marginBottom = 50;
-      const marginLeft = 50;
-
-      const series = d3.stack()
-        .keys(d3.union(data.map(d => d.industry)))
-        .value(([, D], key) => D.get(key)?.unemployed || 0)
-        (d3.index(data, d => d.date, d => d.industry));
-
-      const x = d3.scaleUtc()
-        .domain(d3.extent(data, d => d.date))
-        .range([marginLeft, width - marginRight]);
-
-      const y = d3.scaleLinear()
-        .domain([0, d3.max(series, d => d3.max(d, d => d[1]))])
-        .rangeRound([height - marginBottom, marginTop]);
-
-      // const color = d3.scaleOrdinal()    //使用分类颜色方案
-      //   .domain(series.map(d => d.key))
-      //   .range(d3.schemeTableau10);
-        // .range(d3.schemeCategory10);
-        // .range(d3.schemeAccent);
-        // .range(d3.schemeDark2);
-        // .range(d3.schemePaired);
-        // .range(d3.schemePastel1);
-        // .range(d3.schemeSet3);
-
-      // 将每个类别映射到一个数值
-      const categoryToNumber = d3.scaleOrdinal()    //使用渐变颜色方案
-        .domain(series.map(d => d.key))
-        .range(d3.range(series.length));
-      // 创建顺序颜色比例
-      const color = d3.scaleSequential()
-        .domain([0, series.length - 1])
-        .interpolator(d3.interpolateMagma);
-
-            const area = d3.area()
-              .x(d => x(d.data[0]))
-              .y0(d => y(d[0]))
-              .y1(d => y(d[1]));
-
-      const svg = d3.select(this.$refs.chart)
-        .append("svg")
-        .attr("id", "stacked-line-chart")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", [0, 0, width, height])
-        .attr("style", "max-width: 100%; height: 100%;");
-
-      svg.append("g")
-        .attr("transform", `translate(${marginLeft},0)`)
-        .call(d3.axisLeft(y).ticks(height / 80))
-        .call(g => g.select(".domain").remove())
-        .call(g => g.selectAll(".tick line").clone()
-          .attr("x2", width - marginLeft - marginRight)
-          .attr("stroke-opacity", 0.1))
-          .style("font-size", "20px")
-        // .call(g => g.append("text")
-        //   .attr("x", -marginLeft)
-        //   .attr("y", 10)
-        //   .attr("fill", "currentColor")
-        //   .attr("text-anchor", "start")
-        //   .text("↑ Unemployed persons"));
-
-        const xAxis = d3.axisBottom(x).tickSizeOuter(0).tickFormat(d3.timeFormat("%Y.%m.%d"));
-        svg.append("g")
-        .attr("transform", `translate(0,${height - marginBottom})`)
-        .style("font-size", "16px")
-        .call(xAxis)
-        .call(g => g.select(".domain").remove())
-        .call(g => g.selectAll(".tick line").clone()
-          .attr("y2", -height + marginTop + marginBottom)
-          .attr("stroke-opacity", 0.1))
-        .call(g => g.selectAll(".tick text")
-          .style("font-size", "20px"));
-
-        const paths = svg.append("g")
-        .selectAll("path")
-        .data(series)
-        .join("path")
-        // .attr("fill", d => color(d.key)) //使用分类颜色方案
-        .attr("fill", d => color(categoryToNumber(d.key))) //使用渐变颜色方案
-        .attr("d", area)
-        .attr("class", "area");
-
-      // 添加工具提示框
-      const tooltip = svg.append("g")
-        .attr("id", "tooltip")
-        .style("display", "none");
-
-      tooltip.append("rect")
-        .attr("width", 150)
-        .attr("height", 40)
-        .attr("fill", "white")
-        .attr("stroke", "black");
-
-      tooltip.append("text")
-        .attr("x", 80)
-        .attr("y", 20)
-        .attr("dy", ".35em")
-        .attr("text-anchor", "middle");
-
-      // 添加交互效果
-      paths
-        .on("mouseover", function(event, d) {
-          d3.selectAll(".area").style("opacity", 0.1);
-          d3.select(this).style("opacity", 1);
-          tooltip.style("display", null);
-        })
-        .on("mousemove", function(event, d) {
-          const [mouseX, mouseY] = d3.pointer(event, svg.node());
-          tooltip.attr("transform", `translate(${mouseX + 10},${mouseY + 20})`);
-          tooltip.select("text").text(d.key);
-        })
-        .on("mouseout", function() {
-          d3.selectAll(".area").style("opacity", 1);
-          tooltip.style("display", "none");
-        });
-
-      // 添加图例
-      const legend = svg.append("g")
-        .attr("transform", `translate(${marginRight + 70},${marginTop})`)
-        .attr("class", "legend");
-
-      // 设置每行显示的图例项数量
-      const itemsPerRow = 5;
-
-      // 计算图例项的宽度和高度
-      const legendItemWidth = 150;
-      const legendItemHeight = 30;
-      
-      const legendItems = legend.selectAll(".legend-item")
-        .data(series)
-        .enter()
-        .append("g")
-        .attr("class", "legend-item")
-        // .attr("transform", (d, i) => `translate(0,${i * 20})`);
-        .attr("transform", (d, i) => {
-          const x = (i % itemsPerRow) * legendItemWidth;
-          const y = Math.floor(i / itemsPerRow) * legendItemHeight;
-          return `translate(${x},${y})`;
-        });
-
-      legendItems.append("rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", 24)
-        .attr("height", 24)
-        // .attr("fill", d => color(d.key)); //使用分类颜色方案
-        .attr("fill", d => color(categoryToNumber(d.key))); //使用渐变颜色方案
-
-      legendItems.append("text")
-        .attr("x", 30)
-        .attr("y", 12)
-        .attr("dy", ".35em")
-        .text(d => d.key);
-    }
-  }
+  },
 };
 </script>
 
-<style>
+<style scoped>
 .chart-container {
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-
+</style>
