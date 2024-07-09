8bf3ea9906fe248fa391b7b8c4e62c729d20a27a
HYiism <aishiminhy@163.com>
2024-07-09 10:57:26 +0800

    修改bug

--- src/components/StudentViewer/RightViewerStudent.vue
+++ src/components/StudentViewer/RightViewerStudent.vue
@@ -4,10 +4,11 @@
     <div class="chat-box" ref="chatBox" v-html="renderedText"></div>
   </div>
 </template>
+
 <script>
-import { html } from 'd3';
 import MarkdownIt from 'markdown-it';
 import { mapGetters } from 'vuex';
+import PubSub from 'pubsub-js';
 
 export default {
   name: 'ChatBox',
@@ -16,84 +17,82 @@ export default {
       avatar: 'https://statics.moonshot.cn/kimi-chat/static/01.0245bc9d.png',
       text: '',
       typingSpeed: 10,
-      // 用来显示的数据
       displayedText: '',
-      // 用来显示的markdown文本 实时修改 displayedText
-      renderedText: ''
+      renderedText: '',
+      requestQueue: [], // 队列来存储请求ID
+      isFetching: false, // 当前是否正在进行请求
     };
   },
   mounted() {
     this.subscriptionToken = PubSub.subscribe('studentId', (msg, value) => {
-      this.fetchTextStream(value);
+      this.addToQueue(value);
     });
   },
   computed: {
-    ...mapGetters(['studentId'])
+    ...mapGetters(['studentId']),
   },
   created() {
-    this.fetchTextStream();
+    this.addToQueue(this.studentId);
   },
   methods: {
-
-    fetchTextStream() {
-      let _this = this;
-
-      fetch(`http://10.12.44.190:8000/getComments_stream/?student_id=${this.studentId}`, {
-        method: 'GET',
-      }).then(response => {
-        const reader = response.body.getReader();
-        const decoder = new TextDecoder('utf-8');
-
-        function processStreamResult(result) {
-          const chunk = decoder.decode(result.value, { stream: !result.done });
-          // 可以接收到异步数据！
-          console.log('Received chunk:', chunk);
-          // 成功显示在前端页面！现在的问题是前端显示太快了，需要加一个延时显示
-          _this.displayedText += chunk;
-          // _this.text += chunk;
-          // showText中慢慢将text倒入到displayedText中
-          // _this.showText();
-          _this.renderMarkdown();
-          _this.scrollToBottom();
-
-          if (!result.done) {
-            reader.read().then(processStreamResult);
-          }
-        }
-        reader.read().then(processStreamResult);
-      })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-
+    addToQueue(studentId) {
+      this.requestQueue.push(studentId);
+      this.processQueue();
     },
-    // showText() {
-    //   let index = 0;
-    //   const textLength = this.text.length;
-    //   const interval = setInterval(() => {
-    //     this.displayedText += this.text.charAt(index);
-    //     index++;
-    //     this.scrollToBottom();
-    //     if (index === textLength) {
-    //       clearInterval(interval);
-    //     }
-    //   }, this.typingSpeed);
-    //   // 导入到displayedText后，再进行markdown渲染
-    //   this.renderMarkdown();
-    //   this.scrollToBottom();
-    // },
+    processQueue() {
+      if (this.isFetching || this.requestQueue.length === 0) return;
+
+      this.isFetching = true;
+      const studentId = this.requestQueue.shift();
+      this.fetchTextStream(studentId);
+    },
+    fetchTextStream(studentId) {
+      fetch(`http://10.12.44.190:8000/getComments_stream/?student_id=${studentId}`, {
+        method: 'GET',
+      })
+          .then(response => {
+            const reader = response.body.getReader();
+            const decoder = new TextDecoder('utf-8');
+
+            const processStreamResult = (result) => {
+              const chunk = decoder.decode(result.value, { stream: !result.done });
+              console.log('Received chunk:', chunk);
+              this.displayedText += chunk;
+              this.renderMarkdown();
+              this.scrollToBottom();
+
+              if (!result.done) {
+                reader.read().then(processStreamResult);
+              } else {
+                this.isFetching = false; // 请求完成
+                this.processQueue(); // 处理下一个请求
+              }
+            };
+
+            reader.read().then(processStreamResult);
+          })
+          .catch(error => {
+            this.isFetching = false; // 确保请求错误时也能继续处理队列
+            if (error.name === 'AbortError') {
+              console.log('Fetch aborted');
+            } else {
+              console.error('Error:', error);
+            }
+            this.processQueue(); // 处理下一个请求
+          });
+    },
     renderMarkdown() {
-      const md = new MarkdownIt({html: true, linkify: true, typographer: true, breaks: true});
+      const md = new MarkdownIt({ html: true, linkify: true, typographer: true, breaks: true });
       this.renderedText = md.render(this.displayedText);
-      // this.renderedText = this.displayedText;
     },
     scrollToBottom() {
       const chatBox = this.$refs.chatBox;
       chatBox.scrollTop = chatBox.scrollHeight;
-    }
+    },
   },
 };
 </script>
+
 <style scoped>
 .chat-container {
   display: flex;
@@ -110,13 +109,10 @@ export default {
   background-color: #f0f0f0;
   border-radius: 10px;
   padding: 10px;
-
   max-width: 580px;
   max-height: 700px;
   overflow-y: auto;
-  /* 下面两行代码导致间距过大 */
   word-break: break-word;
-  /* white-space: pre-wrap;  */
   font-family: Arial, sans-serif;
 }
 </style>
